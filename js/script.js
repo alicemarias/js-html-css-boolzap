1360720d09ad7c08a9fc136519970a0daf55ecce
alice maria spina <alicem.spina93@gmail.com>
2020-06-17 14:40:44 +0200

    commit js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+$(document).ready(function() {
+  // quando clicco su aeroplanino per inviare mex
+  $('button.invio-mex').click(function() {
+    sendMessage();
+  });
+  // funzione per creare e mandare messaggio
+  function sendMessage() {
+  var messaggio = $('input-messaggio input').val();
+  if(messaggio != '') {
+    //clono template del massaggio
+    var messaggioNuovo = $('.conversazioni .mex').clone();
+    // ci agiungo testo della input
+    messaggioNuovo.children('.testo-messaggio').text(messaggio);
+    // aggiungo classe inviato
+    messaggioNuovo.addClass('inviato');
 
+    // appendo nuovo messagio alla chat
+    $('.finestra-chat').append(messaggioNuovo);
+  }
+  }
+});
