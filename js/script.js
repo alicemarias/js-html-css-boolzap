cbe61a86ca8fe20831c8eb1f1e99deb7b28eeb62
alice maria spina <alicem.spina93@gmail.com>
2020-06-18 11:59:31 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -8,11 +8,18 @@ $(document).ready(function() {
   var messaggio = $('.input-messaggio input').val();
   if(messaggio != '') {
     //clono template del massaggio
-    var messaggioNuovo = $('.conversazioni .mex').clone();
+    var messaggioNuovo = $('.conversazioni .messaggio').clone();
     // ci agiungo testo della input
     messaggioNuovo.children('.testo-messaggio').text(messaggio);
     // aggiungo classe inviato
     messaggioNuovo.addClass('inviato');
+
+    //inserisco orario messaggio
+    var date = new Date();
+    var currentHours = date.getHours();
+    var currentMinutes = date.getMinutes();
+    var currentTime = currentHours + ':' + currentMinutes;
+    messaggioNuovo.children('.tempo-messaggio').text(currentTime);
 
     // appendo nuovo messagio alla chat
     $('.finestra-chat').append(messaggioNuovo);
